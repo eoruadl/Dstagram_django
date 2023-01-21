da88eadd678f793e6378b20cb564ff2dfec2ac1d
eoruadl <108312272+eoruadl@users.noreply.github.com>
2023-01-21 17:33:41 +0900

    댓글 기능 구현 및 권한 제한

--- photo/views.py
+++ photo/views.py
@@ -4,11 +4,15 @@ from .models import Photo
 
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+@login_required
 def photo_list(request):
     photos = Photo.objects.all()
     return render(request, 'photo/list.html', {'photos':photos})
 
-class PhotoUploadView(CreateView):
+class PhotoUploadView(LoginRequiredMixin, CreateView):
     model = Photo
     fields = ['photo', 'text']
     template_name = 'photo/upload.html'
@@ -21,12 +25,12 @@ class PhotoUploadView(CreateView):
         else:
             return self.render_to_response({'form':form})
 
-class PhotoDeleteView(DeleteView):
+class PhotoDeleteView(LoginRequiredMixin ,DeleteView):
     model = Photo
     success_url = '/'
     template_name = 'photo/delete.html'
 
-class PhotoUpdateView(UpdateView):
+class PhotoUpdateView(LoginRequiredMixin, UpdateView):
     model = Photo
     fields = ['photo', 'text']
     template_name = 'photo/update.html'
